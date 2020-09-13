419bf60bd56ef05395ca982b5cbd1630ae56d3ce
Bastien Lemaire <blemaire@users.noreply.github.com>
2020-09-13 11:33:34 +0200

    Merge pull request #2 from blemaire/develop
    Test heroku automated deployment

--- packages/thal-bot/src/index.ts
+++ packages/thal-bot/src/index.ts
@@ -1,13 +1,26 @@
-import * as tmi from 'tmi.js';
-import {config} from './config';
-import {App} from './app';
+// import * as tmi from 'tmi.js';
+// import {config} from './config';
+// import {App} from './app';
+//
+// const client = tmi.client(config.config.twitter);
+//
+// new App(client).start();
+//
+// client.on('connected', function onConnectedHandler(addr, port) {
+//   console.log(`* Connected to ${addr}:${port}`);
+// });
+//
+// client.connect();
 
-const client = tmi.client(config.config.twitter);
 
-new App(client).start();
+const express = require('express')
+const app = express()
+const port = 80
 
-client.on('connected', function onConnectedHandler(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-});
+app.get('/', (req: any, res: any) => {
+  res.send('Hello World!')
+})
 
-client.connect();
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
