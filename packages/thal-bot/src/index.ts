bbea9e7b01be189e487fc8901ba1ad57b36fd943
Bastien Lemaire <blemaire@users.noreply.github.com>
2020-09-13 14:14:22 +0200

    Merge pull request #7 from blemaire/develop
    Simple change to test heroku deployment

--- packages/thal-bot/src/index.ts
+++ packages/thal-bot/src/index.ts
@@ -19,5 +19,5 @@ import express from 'express';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.get('/', (req: any, res: any) => res.send('Hello World!'));
+app.get('/', (req: any, res: any) => res.send('Hello World! from master'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
