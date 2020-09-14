c700ade9d8794f848901507bd2071dcf71ef7d63
Bastien Lemaire <blemaire@users.noreply.github.com>
2020-09-14 18:16:58 +0200

    Merge pull request #13 from blemaire/develop
    Added participants list to the message

--- packages/thal-bot/src/endo-war/endo-war.controller.ts
+++ packages/thal-bot/src/endo-war/endo-war.controller.ts
@@ -1,6 +1,6 @@
 import {Controller, Get} from '@nestjs/common';
 import {ApiResponse, ApiTags} from '@nestjs/swagger';
-import {Browser, launch} from 'puppeteer';
+import {Browser, launch, Page} from 'puppeteer';
 import {Observable, Observer} from 'rxjs';
 import {TextResponseDto} from '../common/text-response';
 import {EndoWarService} from './endo-war.service';
@@ -17,39 +17,46 @@ export class EndoWarController {
     type: TextResponseDto,
   })
   @Get('status')
-  getHello(): Observable<TextResponseDto> {
+  public getStatus(): Observable<TextResponseDto> {
     return new Observable((observer: Observer<TextResponseDto>) => {
-      launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-      })
-      .then((browser: Browser) => {
-        browser.newPage().then(page => {
-          page.goto('https://www.leetchi.com/c/endo-war').then(_page => {
-            page.evaluate(() =>
-                [
-                  document.querySelector('.col-sidebar .panel-heading .panel-title')?.textContent,
-                  document.querySelector('.col-sidebar .status__column.delay')?.textContent,
-                  document.querySelector('.col-sidebar .status__column.contribution')?.textContent
-                ]
-            )
-            .then(parts => {
-              observer.next({
-                text: parts
-                .map(part =>
-                  part
-                  .replace(/(\r\n|\n|\r)/gm, ' ')
-                  .trim()
-                  .replace(/\s+/g, ' ')
-                )
+      this.openCampaign().then(({page, browser}) => {
+        page.evaluate(() =>
+            [
+              'Campagne Endo-War: ' + document.querySelector('.col-sidebar .panel-heading .panel-title')?.textContent,
+              document.querySelector('.col-sidebar .status__column.contribution')?.textContent,
+              'Derniers participants: ' + Array.from(document.querySelectorAll('.fdr-contributor-name')).slice(0, 5).map(element => element.textContent).join(', '),
+              // document.querySelector('.col-sidebar .status__column.delay')?.textContent,
+              'https://www.leetchi.com/c/endo-war',
+            ]
+          )
+          .then(parts => {
+            observer.next({
+              text: parts
+                .map(this.sanitiseText)
                 .join('. ')
-              });
-              observer.complete();
-
-              return browser.close();
             });
+            observer.complete();
+
+            return browser.close();
           });
-        });
       });
     });
+  }
+
+  private openCampaign(): Promise<{ page: Page, browser: Browser }> {
+    return launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+      .then((browser: Browser) => {
+        return browser.newPage().then(page => {
+          return page.goto('https://www.leetchi.com/c/endo-war').then(() => ({page, browser}));
+        });
+      });
+  }
+
+  private sanitiseText(text: string): string {
+    return text.replace(/(\r\n|\n|\r)/gm, ' ')
+      .trim()
+      .replace(/\s+/g, ' ');
   }
 }
