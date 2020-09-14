d7308fb6a7078c962f2e5cf7ddfb9fe2ee70cf60
Bastien Lemaire <blemaire@users.noreply.github.com>
2020-09-14 15:59:01 +0200

    Merge pull request #12 from blemaire/develop
    Added puppeteer typings, simplified webScraping logic, Added more information on swagger

--- packages/thal-bot/src/endo-war/endo-war.controller.ts
+++ packages/thal-bot/src/endo-war/endo-war.controller.ts
@@ -1,8 +1,9 @@
 import {Controller, Get} from '@nestjs/common';
+import {ApiResponse, ApiTags} from '@nestjs/swagger';
+import {Browser, launch} from 'puppeteer';
 import {Observable, Observer} from 'rxjs';
-import * as puppeteer from 'puppeteer';
+import {TextResponseDto} from '../common/text-response';
 import {EndoWarService} from './endo-war.service';
-import {ApiTags} from '@nestjs/swagger';
 
 @ApiTags('endo-war')
 @Controller('endo-war')
@@ -10,31 +11,45 @@ export class EndoWarController {
   constructor(private readonly endoWarService: EndoWarService) {
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'A summary of the Endo-War money-pool campaign progress',
+    type: TextResponseDto,
+  })
   @Get('status')
-  getHello(): Observable<string> {
-    return new Observable((observer: Observer<string>) => {
-      puppeteer.launch({
-          args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-          ],
-        })
-        .then(browser => {
-          browser.newPage()
-            .then(page => {
-              page.goto('https://www.leetchi.com/c/endo-war').then(_page => {
-                Promise.all([
-                  page.evaluate(() => document.querySelector('.col-sidebar .panel-heading .panel-title')?.textContent),
-                  page.evaluate(() => document.querySelector('.col-sidebar .status__column.delay')?.textContent),
-                  page.evaluate(() => document.querySelector('.col-sidebar .status__column.contribution')?.textContent),
-                ]).then(parts => {
-                  observer.next(parts.map(part => part.replace(/(\r\n|\n|\r)/gm, ' ').trim().replace(/\s+/g, ' ')).join('. '));
-                  observer.complete();
-                  return browser.close();
-                });
+  getHello(): Observable<TextResponseDto> {
+    return new Observable((observer: Observer<TextResponseDto>) => {
+      launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
+      .then((browser: Browser) => {
+        browser.newPage().then(page => {
+          page.goto('https://www.leetchi.com/c/endo-war').then(_page => {
+            page.evaluate(() =>
+                [
+                  document.querySelector('.col-sidebar .panel-heading .panel-title')?.textContent,
+                  document.querySelector('.col-sidebar .status__column.delay')?.textContent,
+                  document.querySelector('.col-sidebar .status__column.contribution')?.textContent
+                ]
+            )
+            .then(parts => {
+              observer.next({
+                text: parts
+                .map(part =>
+                  part
+                  .replace(/(\r\n|\n|\r)/gm, ' ')
+                  .trim()
+                  .replace(/\s+/g, ' ')
+                )
+                .join('. ')
               });
+              observer.complete();
+
+              return browser.close();
             });
+          });
         });
+      });
     });
   }
 }
