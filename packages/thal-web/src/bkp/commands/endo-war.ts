c3c27b4e38d4e5989b72c9d931a2702382c3aaca
blemaire <bastien.lemaire@gmail.com>
2020-10-20 15:46:56 +0200

    Used jsDom to parse the html for endoWar

--- packages/thal-web/src/bkp/commands/endo-war.ts
+++ packages/thal-web/src/bkp/commands/endo-war.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 import * as tmi from 'tmi.js';
 import {ICommand} from './abstract';
 
@@ -15,17 +15,18 @@ export class EndoWarCommand implements ICommand {
   }
 
   public async exec(target: string, context: Record<string, string>, msg: string, ...args: string[]): Promise<void> {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    page.on('domcontentloaded', async () => {
-      const amount = await page.evaluate(() => document.querySelector('.col-sidebar .panel-heading .panel-title')?.textContent);
-      const status = await page.evaluate(() => document.querySelector('.col-sidebar .status__column.delay')?.textContent);
-      const contributions = await page.evaluate(() => document.querySelector('.col-sidebar .status__column.contribution')?.textContent);
-      this.client.say(target, `${amount}. ${status}. ${contributions}`);
-    });
-
-    await page.goto('https://www.leetchi.com/c/endo-war');
-    await browser.close();
+    this.client.say(target, `Test command`);
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    //
+    // page.on('domcontentloaded', async () => {
+    //   const amount = await page.evaluate(() => document.querySelector('.col-sidebar .panel-heading .panel-title')?.textContent);
+    //   const status = await page.evaluate(() => document.querySelector('.col-sidebar .status__column.delay')?.textContent);
+    //   const contributions = await page.evaluate(() => document.querySelector('.col-sidebar .status__column.contribution')?.textContent);
+    //   this.client.say(target, `${amount}. ${status}. ${contributions}`);
+    // });
+    //
+    // await page.goto('https://www.leetchi.com/c/endo-war');
+    // await browser.close();
   }
 }
